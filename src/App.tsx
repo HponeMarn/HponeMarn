ec0e8d9e4fd7ddcd34cd3b9760fbf3c46356bf82
HponeMarn <supermanph757@gmail.com>
2026-05-21 18:33:36 +0630

    modify app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 
 import './App.css'
 import About from './components/About';
-import Contact from './components/Contact';
-import Education from './components/Education';
-import Experience from './components/Experience';
-
+import Contact from './components/contact';
+import Education from './components/education';
+import Experience from './components/experience';
 import Footer from './components/Footer'
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
+import Hero from './components/hero';
+import Navbar from './components/navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 
